ea1f935dcf4e0999ddf3c1d82d9c619b7886c77c
morimorig3 <mori.nto@outlook.jp>
2023-07-23 16:22:00 +0900

    feat:Headerコンポーネントの実装

---
+++ frontend/src/lib/components/Header.svelte
@@ -0,0 +1,5 @@
+<header class="shadow-sm">
+    <div class="h-14 md:container mx-auto px-4 flex items-center">
+        <p class="font-bold">Svelte TODO with GO BackEnd</p>
+    </div>
+</header>
--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,9 +1,10 @@
 <script>
-	import '../app.css';
+	import Header from '$lib/components/Header.svelte';
+import '../app.css';
 </script>
 
 <div class="">
-	<header>ヘッダー</header>
+	<Header />
 	<main>
 		メイン
 		<slot />
