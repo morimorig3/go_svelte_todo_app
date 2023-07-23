dab2cc158d32861c20b7f3d4acf300a18f408a28
morimorig3 <mori.nto@outlook.jp>
2023-07-23 17:46:24 +0900

    feat:タスクリストコンポーネントの実装

---
+++ frontend/src/lib/components/TaskList.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import type { Task as TaskType } from '$lib/types';
+	import Task from './Task.svelte';
+
+	export let tasks: TaskType[] = [];
+</script>
+
+{#if tasks.length > 0}
+	<ul>
+		{#each tasks as { id, title } (id)}
+			<Task {id} {title} />
+		{/each}
+	</ul>
+{:else}{/if}
--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,10 +1,25 @@
-<script>
+<script lang="ts">
 	import InputTask from '$lib/components/InputTask.svelte';
+	import TaskList from '$lib/components/TaskList.svelte';
+	import type { Task } from '$lib/types';
+
+	const tasks: Task[] = [
+		{
+			id: '1',
+			title: 'タスク1'
+		},
+		{
+			id: '2',
+			title: 'タスク2'
+		},
+		{
+			id: '3',
+			title: 'タスク3'
+		}
+	];
 </script>
 
-<InputTask />
-<ul>
-	<li>TODO</li>
-	<li>TODO</li>
-	<li>TODO</li>
-</ul>
+<div class="flex flex-col gap-y-10">
+	<InputTask />
+	<TaskList {tasks} />
+</div>
