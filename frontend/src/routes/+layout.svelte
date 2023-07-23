4a392915cbd708728cfdea8813560c69adc2cf2b
morimorig3 <mori.nto@outlook.jp>
2023-07-23 15:59:09 +0900

    feat:tailwind追加

---
+++ frontend/src/routes/+layout.svelte
@@ -0,0 +1,17 @@
+<script>
+	import '../app.css';
+</script>
+
+<div class="">
+	<header>ヘッダー</header>
+	<main>
+		メイン
+		<slot />
+	</main>
+	<footer>フッター</footer>
+</div>
+
+<style lang="postcss">
+	:global(html) {
+	}
+</style>
