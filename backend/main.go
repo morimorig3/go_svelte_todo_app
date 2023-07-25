62269a751d83cfb4966456f83cd57c3d8df4a888
morimorig3 <mori.nto@outlook.jp>
2023-07-25 21:32:55 +0900

    feat:ポートの動的割り当て

--- backend/main.go
+++ backend/main.go
@@ -4,21 +4,22 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"net"
 	"net/http"
+	"os"
 
 	"golang.org/x/sync/errgroup"
 )
 
-func run(ctx context.Context) error {
+func run(ctx context.Context, l net.Listener) error {
 	s := &http.Server{
-		Addr: ":18080",
 		Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
 		}),
 	}
 	eg, ctx := errgroup.WithContext(ctx)
 	eg.Go(func() error {
-		err := s.ListenAndServe();
+		err := s.Serve(l);
 		if err != nil && err != http.ErrServerClosed {
 			log.Printf("failed to close: %+v", err)
 			return err
@@ -34,7 +35,16 @@ func run(ctx context.Context) error {
 }
 
 func main() {
-	err := run(context.Background())
+	if len(os.Args) != 2 {
+		log.Printf("need port \n")
+		os.Exit(1)
+	}
+	p := os.Args[1]
+	l, err := net.Listen("tcp", ":"+p)
+	if err != nil {
+		log.Fatalf("failed to listen port %s:%v", p, err)
+	}
+	err = run(context.Background(), l)
 	if err != nil {
 		log.Printf("failed to terminate server: %v", err)
 	}
