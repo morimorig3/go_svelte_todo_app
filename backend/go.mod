d95d9fc5aeb0ecb27d4306333139a824fc6b7b4b
morimorig3 <mori.nto@outlook.jp>
2023-07-25 13:39:09 +0900

    feat:run関数切り出し

--- backend/go.mod
+++ backend/go.mod
@@ -1,3 +1,8 @@
 module github.com/morimorig3/go_svelte_todo_app/backend
 
 go 1.20
+
+require (
+	github.com/caarlos0/env/v6 v6.10.1
+	golang.org/x/sync v0.3.0
+)
--- backend/main.go
+++ backend/main.go
@@ -1,20 +1,41 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"log"
 	"net/http"
-	"os"
+
+	"golang.org/x/sync/errgroup"
 )
 
-func main() {
-	err := http.ListenAndServe(
-		":8080",
-		http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+func run(ctx context.Context) error {
+	s := &http.Server{
+		Addr: ":18080",
+		Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
 		}),
-	)
+	}
+	eg, ctx := errgroup.WithContext(ctx)
+	eg.Go(func() error {
+		err := s.ListenAndServe();
+		if err != nil && err != http.ErrServerClosed {
+			log.Printf("failed to close: %+v", err)
+			return err
+		}
+		return nil
+	})
+	<-ctx.Done()
+	err := s.Shutdown(context.Background());
 	if err != nil {
-		fmt.Printf("failed to terminate server: %v", err)
-		os.Exit(1)
+		log.Printf("failed to shutdown: %+v", err)
+	}
+	return eg.Wait()
+}
+
+func main() {
+	err := run(context.Background())
+	if err != nil {
+		log.Printf("failed to terminate server: %v", err)
 	}
 }
